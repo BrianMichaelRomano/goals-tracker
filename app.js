8bcef61229611cebee473a4f4a2b97178c18612b
Brian <brianmromano@gmail.com>
2018-12-07 07:00:56 -0500

    added env variables for port and database

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
 const rootDir = require('./util/rootDir.js');
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.DB_URI;
 
 const authRoutes = require('./routes/auth.js');
 const chartRoutes = require('./routes/charts.js');
@@ -21,4 +25,9 @@ app.use((req, res, next) => {
   res.render('index');
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+mongoose.connect(DB_URI)
+  .then(() => {
+    console.log('Connected to Database...');
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+  })
+  .catch((err) => console.log(err));
