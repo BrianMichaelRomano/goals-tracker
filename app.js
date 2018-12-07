f5676093bc460dc25fe50d05974d8e1643202f56
Brian <brianmromano@gmail.com>
2018-12-07 07:27:14 -0500

    added goal model

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   res.render('index');
 });
 
-mongoose.connect(DB_URI)
+mongoose.connect(DB_URI, { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Database...');
     app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
