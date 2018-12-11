7a117e57c7a7feb8705c6860b253fb1a7537bee8
Brian <brianmromano@gmail.com>
2018-12-10 20:22:27 -0500

    added signup and login post routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,16 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login');
 };
 
+exports.postLogin = (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/charts/dashboard');
+};
+
 exports.getSignup = (req, res, next) => {
   res.render('auth/signup');
+};
+
+exports.postSignup = (req, res, next) => {
+  console.log('Signup', req.body)
+  res.redirect('login');
 };
