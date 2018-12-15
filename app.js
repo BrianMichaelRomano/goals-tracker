75e3a0e81840269cba6b8a72826e708e0f876ca2
Brian <brianmromano@gmail.com>
2018-12-15 11:11:49 -0500

    completed and updated csrf issues

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isAuthenticated;
-  res.locals.csfrToken = req.csrfToken();
+  res.locals.csrfToken = req.csrfToken();
   next();
 });
 
