e8c83f406fecb78cdda819d40c49499853edeb38
Brian <brianmromano@gmail.com>
2018-12-15 09:12:05 -0500

    added authGuard

--- routes/charts.js
+++ routes/charts.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const chartsController = require('../controllers/charts.js');
+const authGuard = require('../middleware/authGuard.js');
 
-router.get('/dashboard', chartsController.getChart);
+router.get('/dashboard', authGuard, chartsController.getChart);
 
 module.exports = router;
