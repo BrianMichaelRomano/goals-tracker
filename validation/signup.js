ff54e20bb82e225c0ab7eed0712aecae1c1e8622
Brian <brianmromano@gmail.com>
2018-12-20 19:49:24 -0500

    refactored validation

--- validation/signup.js
+++ validation/signup.js
@@ -4,7 +4,7 @@ const User = require('../models/user.js');
 module.exports = [
   body('name', 'Name must be 3 to 14 characters long and use numbers and text only...')
     .isLength({ min: 3, max: 14 }),
-  body('email', 'Must use a valid email...')
+  body('email', 'Must enter a valid email...')
     .isEmail()
     .custom((value, { req }) => {
       return User.findOne({ email: value })
@@ -13,14 +13,16 @@ module.exports = [
             return Promise.reject('User with this email already exists...');
           }
         })
+        .catch(err => console.log(err));
     }),
   body('password', 'Password must be 6 to 14 characters long and use numbers and text only...')
     .isLength({ min: 6, max: 14 })
     .isAlphanumeric(),
-  body('confirmPassword').custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error('Passwords must match...');
-    }
-    return true;
-  })
+  body('confirmPassword')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords must match...');
+      }
+      return true;
+    })
 ];
