00d5d7e17436f0de3f84096c5712a6e8dd8b6a59
Brian <brianmromano@gmail.com>
2018-12-21 03:50:38 -0500

    reverted error handling for now

--- app.js
+++ app.js
@@ -71,9 +71,7 @@ app.use((req, res, next) => {
       req.user = user;
       next();
     })
-    .catch(err => {
-      throw new Error(err);
-    });
+    .catch(err => console.log(err));
 });
 
 app.use((req, res, next) => {
