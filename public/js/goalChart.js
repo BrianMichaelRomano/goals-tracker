140ce6aa057c896891d611eb85538b6be9e68199
Brian <brianmromano@gmail.com>
2018-12-23 13:06:14 -0500

    goal chart ja file now recieves goalId from url pathname

--- public/js/goalChart.js
+++ public/js/goalChart.js
@@ -1,9 +1,16 @@
-const goalName = document.querySelector('#goalName').value;
-const chartType = document.querySelector('#chartType').value;
-const daysToTrack = document.querySelector('#daysToTrack').value;
-const borderColor = document.querySelector('#borderColor').value;
-const backgroundColor = document.querySelector('#backgroundColor').value;
-const startDate = document.querySelector('#startDate').value;
+const pathArray = window.location.pathname.split('/');
+const goalId = pathArray[pathArray.length - 1];
+let goal;
+
+fetch(`http://localhost:5000/goals/goal/${goalId}`)
+  .then(goal => {
+    return goal.json();
+  })
+  .then(goalData => {
+    renderChart(goalData);
+  })
+  .catch(err => console.log(err));
+
 
 function createChartLables(numOfDays) {
   const result = [];
@@ -13,41 +20,43 @@ function createChartLables(numOfDays) {
   return result;
 };
 
-const chartLabels = createChartLables(daysToTrack);
-const data = {
-  label: goalName,
-  data: [3, 4, 6, 2, 6, 7, 8, 6],
-  backgroundColor: backgroundColor,
-  borderColor: borderColor,
-  borderWidth: 1
-};
+function renderChart(goal) {
+  const chartLabels = createChartLables(goal.daysToTrack);
+  const data = {
+    label: goal.goalName,
+    data: [3, 4, 6, 2, 6, 7, 8, 6],
+    backgroundColor: goal.backgroundColor,
+    borderColor: goal.borderColor,
+    borderWidth: 1
+  };
 
-const ctx = document.getElementById("myChart");
+  const ctx = document.getElementById("myChart");
 
-let myChart = new Chart(ctx, {
-  type: chartType,
-  data: {
-    labels: chartLabels,
-    datasets: [data]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: 'Hours'
-        },
-        ticks: {
-          beginAtZero: true
-        }
-      }],
-      xAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: 'Days'
-        }
-      }]
+  let myChart = new Chart(ctx, {
+    type: goal.chartType,
+    data: {
+      labels: chartLabels,
+      datasets: [data]
     },
-    responsive: true
-  }
-});
+    options: {
+      scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Hours'
+          },
+          ticks: {
+            beginAtZero: true
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Days'
+          }
+        }]
+      },
+      responsive: true
+    }
+  });
+};
