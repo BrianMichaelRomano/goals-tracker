a760466e4838c3e0c0de1469fcb34f36294fbfb2
Brian <brianmromano@gmail.com>
2018-12-31 15:52:48 -0500

    removed images to destination for images

--- app.js
+++ app.js
@@ -61,9 +61,6 @@ app.use(session({
 }));
 
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images');
-  },
   filename: (req, file, cb) => {
     cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
   }
