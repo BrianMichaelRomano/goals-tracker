846a7d44359d00d7feafca84ed883eaeb1b6ce31
Brian <brianmromano@gmail.com>
2019-01-17 07:19:02 -0500

    removed logs

--- public/js/goalChart.js
+++ public/js/goalChart.js
@@ -3,11 +3,9 @@ window['moment-range'].extendMoment(moment);
 var goalId = location.pathname.split('/')[3];
 fetch(`https://sheltered-thicket-56176.herokuapp.com/goals/goal/${goalId}`)
   .then(goalData => {
-    console.log(goalData.json())
     return goalData.json();
   })
   .then(goal => {
-    console.log(goal)
 
     var dataDetails = document.querySelector('#dataDetails');
 
